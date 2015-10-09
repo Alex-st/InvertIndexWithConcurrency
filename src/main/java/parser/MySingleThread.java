04f1fb5b3b75f41746db382026e0c6e2e6717286
Oleksandr_Samosonok <Oleksandr_Samosonok@EPUAKYIW0094.kyiv.epam.com>
2015-10-09 18:56:16 +0300

    2.0

--- src/main/java/parser/MySingleThread.java
+++ src/main/java/parser/MySingleThread.java
@@ -1,18 +1,33 @@
 package parser;
 
-import java.util.LinkedList;
-import java.util.Queue;
+import java.util.HashSet;
+import java.util.Map;
 import java.util.Set;
 import java.util.concurrent.ArrayBlockingQueue;
 import java.util.concurrent.BlockingQueue;
 import java.util.concurrent.Callable;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.locks.Condition;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
 
-public class MySingleThread implements Callable<Set<String>>{
+public class MySingleThread implements Runnable{
 
-	private BlockingQueue<String> queue = new ArrayBlockingQueue<>(1024);
+	private BlockingQueue<String> queue;
+	private Lock lock;
+    private Condition con;
+	private ThreadPoolManager threadPoolManager;
+	
+	public MySingleThread(ThreadPoolManager tpm) {
+		threadPoolManager = tpm;
+		queue = new ArrayBlockingQueue<>(1024);
+		lock = new ReentrantLock();
+	    con = lock.newCondition();
+	}
 	
 	public void addNewLinkToQueue(String link) throws InterruptedException{
 		queue.put(link);
+		con.signal();
 	}
 	
 	public String getLinkFromQueue() throws InterruptedException{
@@ -24,17 +39,35 @@ public class MySingleThread implements Callable<Set<String>>{
 	}
 	
 	@Override
-	public Set<String> call() throws Exception {
+	public void run() {
 		
-		while(queue.size()<1){
-            try {
-                con.await();
-            } catch (InterruptedException ex) {
-                Logger.getLogger(Consumer.class.getName()).log(Level.SEVERE, null, ex);
-            }
-        }
-		// TODO Auto-generated method stub
-		return null;
+		Map<String, String> listOfWords = new ConcurrentHashMap<>();
+		String curLink = null;
+		
+		while (!Thread.currentThread().isInterrupted()) {
+			
+			while(queue.size()<1){
+	            try {
+	                con.await();
+	            } catch (InterruptedException ex) {
+	                //Logger.getLogger(MySingleThread.class.getName()).log(Level.SEVERE, null, ex);
+	            }
+	        }
+			
+			try {
+				curLink = getLinkFromQueue();
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+			
+			for (String i : SoupParser.getAllWordsFromPage(curLink)) {
+				listOfWords.put(i, curLink);
+			}
+			
+			//todo - add import of generated listOfWords to Global Invert List
+		}
+
+		//return listOfWords;
 	}
 
 }
--- src/main/java/parser/ThreadPoolManager.java
+++ src/main/java/parser/ThreadPoolManager.java
@@ -1,5 +1,11 @@
 package parser;
 
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+
 public class ThreadPoolManager {
 
+	private Map<String, String> invertIndex = new ConcurrentHashMap<>();
+	
+	
 }
